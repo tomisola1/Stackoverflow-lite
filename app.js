72fd2bad01bb9983c6e343ce12c280b2f918b282
Solomon Oluwatomisola <oluwatomisola@ext.kobo360.com>
2022-05-24 10:58:08 +0100

    first commit on this project

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import userRoute from "./src/user/userRoute.js";
+
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
+app.use('/user', userRoute)
+
+const port = process.env.PORT || '6000';
+app.listen(port, () =>{
+   console.log(`Server is listening on port ${port}`)
+});
+
+export default app;
